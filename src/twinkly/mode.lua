71d380cde16c10126f305c2d253f0cff65b6a052
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2025-10-04 00:03:12 +0200

    fix: use cosock, messages.

--- src/twinkly/mode.lua
+++ src/twinkly/mode.lua
@@ -1,12 +1,14 @@
-local http = require "socket.http"
+local cosock = require "cosock"
+local http = cosock.asyncify("socket.http")
 local ltn12 = require "ltn12"
 local json = require "dkjson"
 local login = require "twinkly.login"
+local log = require "log"
 
 local mode = {}
 
 function mode.get_mode(ip)
-  print("[get_mode] Entering with IP: " .. tostring(ip))
+  log.debug("[get_mode] Entering with IP: " .. tostring(ip))
   local token, err = login.ensure_token(ip)
   if not token then
     return nil, "No token: " .. tostring(err)
@@ -31,13 +33,8 @@ function mode.get_mode(ip)
   end
 
   local raw_mode = decoded.mode
-  if raw_mode == "off" then
-    print("[get_mode] Normalized mode: off")
-    return "off", body
-  else
-    print("[get_mode] Normalized mode: on (raw was " .. tostring(raw_mode) .. ")")
-    return "on", body
-  end
+  log.debug("[get_mode] raw mode: " .. tostring(raw_mode))
+  return raw_mode
 end
 
 return mode
